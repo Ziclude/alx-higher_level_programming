c0c796b00ebf39bf7cc09cc45b5000871f3a3ec4
ziclude <cindyzickziemi@gmail.com>
2022-05-31 03:20:18 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ cat = 0
 while cat <= 89:
     if cat % 10 == 0:
         cat += 1 + cat // 10
-    print("{:02d}".format(cat), end='\n' if cat == 89 else ",")
+    print("{:02d}".format(cat), end='\n' if cat == 89 else ", ")
     cat += 1
