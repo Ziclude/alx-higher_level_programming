8c5c48f72a339de58af3f7b8505a98ffe33e7031
ziclude <cindyzickziemi@gmail.com>
2022-05-31 03:50:46 -0700

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-FZ = "Fizz"
-BZ = "Buzz"
 def fizzbuzz():
     for cat in range(1, 101):
-        if (cat % 3 and cat % 5):
-            print("%s%s" % (FZ, BZ), end=' ')
-        elif (cat % 3):
-            print("%s" % (FZ), end=' ')
-        elif (cat % 5):
-            print("%s" % (BZ), end=' ')
-        else:
-            print("%d" % (cat), end=' ')
+        if cat % 3 == 0:
+            print("Fizz", end='')
+        if cat % 5 == 0:
+            print("Buzz", end='')
+        if cat % 3 and cat % 5:
+            print("{:d}".format(cat), end='')
+        print(end='')
