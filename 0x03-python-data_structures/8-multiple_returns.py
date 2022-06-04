c8e7663c496ebce83631de053db882760f98139f
ziclude <cindyzickziemi@gmail.com>
2022-06-04 09:54:56 -0700

    tasks 8 and 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    """Returns string's length and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
