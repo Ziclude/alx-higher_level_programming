7bf51b086e61e55b58d99522fde9232922b2d40a
ziclude <cindyzickziemi@gmail.com>
2022-06-08 12:58:49 -0700

    mandatory tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+    aver = 0
+    sz = 0
+    for tp in my_list:
+        aver += (tp[0] * tp[1])
+        sz += tp[1]
+    return (aver / sz)
