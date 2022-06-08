c5d30cd30ec655009c501e73da841f33ad08cce3
ziclude <cindyzickziemi@gmail.com>
2022-06-08 13:37:03 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,14 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    my_dictio = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    my_dictio = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
     numb = 0
     for a in range(len(roman_string)):
         if my_dictio.get(roman_string[a], 0) == 0:
             return 0
-        if (a != (len(roman_string) - 1) and my_dictio[roman_string[a]] < my_dictio[roman_string[a + 1]]):
+        if (a != (len(roman_string) - 1)
+        and my_dictio[roman_string[a]] < my_dictio[roman_string[a + 1]]):
             numb += my_dictio[roman_string[a]] * -1
         else:
             numb += my_dictio[roman_string[a]]
