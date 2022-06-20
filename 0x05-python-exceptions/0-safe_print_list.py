a6cac13b7c4c9b1548d5fec6e3583706785c3360
ziclude <cindyzickziemi@gmail.com>
2022-06-20 03:37:10 -0700

    fix task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[a]), end="")
             tot += 1
         except IndexError:
-            pass
+            break
     print()
     return(tot)
