6e09915686dff78132d05e9293b9be75366fd485
ziclude <cindyzickziemi@gmail.com>
2022-06-20 03:44:21 -0700

    tasks 2 and 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    cat = 0
+    try:
+        cat = a / b
+    except (ZeroDivisionError):
+        cat = None
+    finally:
+        print("Inside result: {}".format(cat))
+        return (cat)
