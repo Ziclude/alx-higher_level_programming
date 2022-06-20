4acf60847a0544eec4c9bc568ee70d7df24364ab
ziclude <cindyzickziemi@gmail.com>
2022-06-20 03:25:35 -0700

    tasks 0 and 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    tot = 0
+    for a in range(x):
+        try:
+            print("{}".format(my_list[a]), end="")
+            tot += 1
+        except IndexError:
+            break
+        print("")
+        return(tot)
