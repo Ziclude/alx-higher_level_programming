b6727e58cadf5356c809f867b8cef57396fb38eb
ziclude <cindyzickziemi@gmail.com>
2022-06-20 04:08:33 -0700

    advanced tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        total = fct(*args)
+        return(total)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def magic_calculation(a, b):
+    cat = 0
+    for u in range(1, 3):
+        try:
+            if u > a:
+                raise Exception('Too far')
+            else:
+                cat += a ** b / u
+        except:
+            cat = b + a
+            break
+    return (cat)
