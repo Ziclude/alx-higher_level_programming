511d33b72567939f942ecff757fa037b040b0783
ziclude <cindyzickziemi@gmail.com>
2022-06-20 04:45:55 -0700

    re:fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,6 @@ def safe_function(fct, *args):
     try:
         total = fct(*args)
         return (total)
-    except:
+    except Exception as error:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
