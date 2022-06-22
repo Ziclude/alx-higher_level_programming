8faae62d0646752b1d60ccb85942f60edb83eff5
ziclude <cindyzickziemi@gmail.com>
2022-06-22 14:04:01 -0700

    Hot fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,11 +10,20 @@ class Square:
         Args:
             size (int): size of the new square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+
+    @property
+    def size(self):
+        """Collect current size of square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """Return current area of the square."""
