7d984ac9f43e62a1793ae8c2412cdbd3c894b72f
ziclude <cindyzickziemi@gmail.com>
2022-06-21 21:02:55 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """Creating a new square.
+        Args:
+        size(int): The size of a new square.
+        """
+        self.__size = size
