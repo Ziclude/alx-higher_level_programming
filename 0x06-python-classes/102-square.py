04924b71e92fb76d76507d5254a67cadd7ac4405
ziclude <cindyzickziemi@gmail.com>
2022-06-22 14:26:09 -0700

    task 10

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,1 +1,54 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Collect current size of square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return current area of the square."""
+        return (self.__size * self.__size)
+
+    def __qe__(self, other):
+        """equals comparison"""
+        return self.area() == other.area()
+
+    def __non__(self, other):
+        """Not comparison"""
+        return self.area() != other.area()
+
+    def __les__(self, other):
+        """inferior comparison"""
+        return self.area() < other.area()
+
+    def __leseq__(self, other):
+        """Inferior or equals comparison"""
+        return self.area() <= other.area()
+
+    def __sup__(self, other):
+        """equals comparison"""
+        return self.area() > other.area()
+
+    def __supeq__(self, other):
+        """Superior or equals comparison"""
+        return self.area() >= other.area()
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""define a MagiClass as the provided bytecode."""
+
+import math
+
+
+class MagiClass:
+    """Circle representation."""
+
+    def __init__(self, radius=0):
+        """Creating a MagiClass.
+        Arg:
+          radius(float or int): radius of MagiClass.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Return MagiClass area"""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Return MagiClass circumference"""
+        return (2 * math.pi * self.__radius)
