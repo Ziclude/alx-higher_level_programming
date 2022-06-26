f3ed84d3a14b6ec4f9373918ad157e89e057e786
ziclude <cindyzickziemi@gmail.com>
2022-06-26 13:16:38 -0700

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""text indentation function."""
+
+
+def text_indentation(text):
+    """Prints two new lines after each specific punctuation
+    Args:
+       text (str): Perfom for operator.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    cat = ['.', '?', ':']
+    nouv_text = ""
+    for ltr in text:
+        nouv_text += ltr
+        if ltr in cat:
+            nouv_text += "\n"
+            print(nouv_text.strip(" "))
+            nouv_text = ""
+    if ltr not in cat:
+        print(nouv_text.strip(" "), end="")
