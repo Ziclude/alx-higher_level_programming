a02bb5f61d900f0b18256c3887e02f4c5b440147
ziclude <cindyzickziemi@gmail.com>
2022-06-27 12:09:51 -0700

    middle tasks

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -39,3 +39,24 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    def area(self):
+        """Area of rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Perimeter of rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return and represents rectangle with # character"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectan = []
+        for a in range(self.__height):
+            [rectan.append('#') for b in range(self.__width)]
+            if a != self.__height - 1:
+                rectan.append("\n")
+        return ("".join(rectan))
---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Represent rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Create a new rectangle.
+        Args:
+           width (int): width of rectangle
+           height (int): height of rectangle
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """Get the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """Get the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """Area of rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Perimeter of rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return and represents rectangle with # character"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectan = []
+        for a in range(self.__height):
+            [rectan.append('#') for b in range(self.__width)]
+            if a != self.__height - 1:
+                rectan.append("\n")
+        return ("".join(rectan))
+
+    def __repr__(self):
+        """String representation of rectangle"""
+        rectan = "Rectangle(" + str(self.__width)
+        rectan += "," + str(self.__height) + ")"
+        return (rectan)
+
+    def __del__(self):
+        """Print message for every deletion"""
+        print("Bye rectangle...")
