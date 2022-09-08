83cdf446359761061815495e91279a39a9b5965e
ziclude <cindyzickziemi@gmail.com>
2022-09-07 20:33:45 -0700

    coding

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+const ls = require('ls');
+const a = ls.readFileSync(process.argv[2], 'utf8');
+const b = ls.readFileSync(process.argv[3], 'utf8');
+ls.writeFileSync(process.argv[4], a + b);
