08cbe093b8be8e673d23c58017d034b72915433b
ziclude <cindyzickziemi@gmail.com>
2022-09-09 16:31:59 -0700

    start coding

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Lists all states from the database."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], pwd=sys.argv[2], db=sys.argv[3])
+    con = db.cursor()
+    con.execute("SELECT * FROM `states`")
+    [print(state) for state in con.fetchall()]
