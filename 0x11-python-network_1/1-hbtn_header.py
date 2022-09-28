e1dfbad80e850b2832303ca1c19fffd940c170e1
ziclude <cindyzickziemi@gmail.com>
2022-09-27 19:27:25 -0700

    task 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Script that url and send request"""
+
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as result:
+        print(dict(result.headers).get("X-Request-Id"))
