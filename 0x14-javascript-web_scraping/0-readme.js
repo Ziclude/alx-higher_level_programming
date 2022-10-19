df3585fbd9c4901671b249260c809cc23e4c6508
ziclude <cindyzickziemi@gmail.com>
2022-10-18 17:50:34 -0700

    changing

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fl = require('fl');
-fl.readFile(process.argv[2], 'utf-8', function (error, content) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (error, content) {
     console.log(error || content);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fl = require('fl');
-fl.writeFile(process.argv[2], process.argv[3], error => {
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
     if (error) console.log(error);
 });
